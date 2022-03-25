bf6632f9d385c06a5999e5a8b8e623197f1c1f6c
Evgeniy Kvach <ypkvach@mts.ru>
2022-03-25 15:22:19 +0300

    adds2

--- index.js
+++ index.js
@@ -101,8 +101,8 @@ var findMedianSortedArrays = function(nums1, nums2) {
 };
 
 var removeElement = function(nums, val) {
-  for ( let i = 0; i < nums.length; i){
-    if(nums[i] === val) {
+  for (let i = 0; i < nums.length; i) {
+    if (nums[i] === val) {
       nums.splice(i, 1);
     } else {
       i++;
@@ -123,11 +123,27 @@ var searchInsert = function(nums, target) {
   return i;
 };
 var maxSubArray = function(nums) {
-
+  let sum;
+  for (let extI = 0; extI < nums.length; extI++) {
+    let res = 0;
+    for (let intI = extI; intI < nums.length; intI++) {
+      sum = (sum && sum !== 0 && intI === extI) ? sum : nums[intI];
+      res = res + nums[intI];
+      if (res > sum) sum = res;
+    }
+  }
+  return sum;
 };
-const nums = [-2,1,-3,4,-1,2,1,-5,4];
-const nums2 = [1];
-const nums3 = [5,4,-1,7,8];
-console.log(maxSubArray(nums)); // 6
-console.log(maxSubArray(nums2)); // 1
-console.log(maxSubArray(nums3)); // 23
+var lengthOfLastWord = function(s) {
+  const str = s
+    .trim()
+    .split(' ')
+    .reverse()
+    .join(' ')
+  const indexOf = str.indexOf(' ');
+  return indexOf === -1 ? str.length : indexOf;
+};
+const s = "a";
+const s2 = "   fly me   to   the moon  "
+console.log(lengthOfLastWord(s)); // 5
+console.log(lengthOfLastWord(s2)); // 4
